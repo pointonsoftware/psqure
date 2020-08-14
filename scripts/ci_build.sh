417c12836aa2cffd395defc2f0f845f9ef0dcbe3
benzgar <garcia.benziv@gmail.com>
2020-08-14 15:25:04 -0400

    Fixed CI build flag

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -2,10 +2,10 @@
 
 # Build core lib
 cd $TRAVIS_BUILD_DIR/packages/lib
-cmake -DUSE_MY_LIB=ON ../../domain
+cmake -DCI_BUILD=ON ../../domain
 make
 
 # Build app bin
 cd ../bin
-cmake -DUSE_MY_LIB=ON ../../application
+cmake -DCI_BUILD=ON ../../application
 make
