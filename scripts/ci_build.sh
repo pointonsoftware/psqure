4ef92d694ffb162bdbba240dc88e9e6e3cc4f4f4
benzgar <garcia.benziv@gmail.com>
2020-08-14 15:22:00 -0400

    Force CI to use gold linker

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -2,10 +2,10 @@
 
 # Build core lib
 cd $TRAVIS_BUILD_DIR/packages/lib
-cmake ../../domain
+cmake -DUSE_MY_LIB=ON ../../domain
 make
 
 # Build app bin
 cd ../bin
-cmake ../../application
+cmake -DUSE_MY_LIB=ON ../../application
 make
